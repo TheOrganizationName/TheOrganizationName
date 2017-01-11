1a75b8f6e506aef772aefa4a7d4a5370173e674a
343137220 <343137220@C-3ZNGYR1.tdsb.on.ca>
2017-01-11 14:25:03 -0500

    first commit

---
+++ TheOrganizationName/src/MainPanel.java
@@ -0,0 +1,27 @@
+import javax.swing.JPanel;
+
+public class MainPanel extends JPanel implements Runnable{
+
+	int width = 1275;
+	int height = 975;
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+	}
+	
+	 
+	public void pacmanStatus() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
