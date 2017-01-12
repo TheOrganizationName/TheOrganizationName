917eaf3f07ef6bafe92067f28d6ed4bef1ba1e33
322425315 <322425315@C-5Y38GQ1.tdsb.on.ca>
2017-01-12 14:59:48 -0500

    almost done the jpanel

--- TheOrganizationName/src/GameWindow.java
+++ TheOrganizationName/src/GameWindow.java
@@ -1,5 +1,21 @@
 import javax.swing.JFrame;
 
 public class GameWindow extends JFrame{
-	private 
+	private ImageIcon image2;
+	private JLabel label1;
+
+	GameWindow(){
+		setLayout(new FlowLayout());
+		
+		image1 = new ImageIcon(getClass().getResource("map.jpg"));
+		
+		label1 = new JLabel(image1);
+		add(label1);
+	}
+	
+	public static void main (String args[]){
+		GameWindow gui = new GameWindow();
+		gui.getDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		gui.setVisible(true);
+	}
 }
