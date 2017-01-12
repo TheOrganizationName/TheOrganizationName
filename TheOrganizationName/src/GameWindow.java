ae259acc29273aa2afad3cb7282331563c1cf8ae
322425315 <322425315@C-5Y38GQ1.tdsb.on.ca>
2017-01-12 14:44:48 -0500

    Pictures

--- TheOrganizationName/src/GameWindow.java
+++ TheOrganizationName/src/GameWindow.java
@@ -1,13 +1,5 @@
-import java.awt.BorderLayout;
+import javax.swing.JFrame;
 
-import javax.swing.ImageIcon;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-
-public class GameWindow {
-	JPanel newPanel = new JPanel();
-	ImageIcon image = new ImageIcon("image/map.jpg");
-	JLabel label = new JLabel("", image, JLabel.CENTER);
-	JPanel panel = new JPanel(new BorderLayout());
-	panel.add (label, BorderLayout.CENTER );
+public class GameWindow extends JFrame{
+	private 
 }
