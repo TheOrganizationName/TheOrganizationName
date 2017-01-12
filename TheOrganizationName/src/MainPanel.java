be143b1d3f69a4b6808c07b289cf94b02dcbf9d1
343137220 <343137220@C-3ZNGYR1.tdsb.on.ca>
2017-01-12 15:06:02 -0500

    MainPanel

--- TheOrganizationName/src/MainPanel.java
+++ TheOrganizationName/src/MainPanel.java
@@ -1,56 +1,40 @@
-import java.awt.Graphics;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
+import java.util.ArrayList;
+
 import javax.swing.JPanel;
 
-public class MainPanel extends JPanel implements Runnable, KeyListener{
+import objects.Student;
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
+public class MainPanel extends JPanel implements Runnable{
+
 	int width = 1275;
 	int height = 975;
 	
 	final int numFood = 1000;
-	arraylist
+	public static ArrayList <PointFruit> listOfStudents= new ArrayList<PointFruit>(numFood);
 	
+	public void pacmanPosition() {
+		// TODO Auto-generated method stub
+		
+	}
 	
+
 	 
 	public void pacmanStatus() {
 		// TODO Auto-generated method stub
 		
 	}
 	
+	int static eatPoint(pacmanPosition, ){
+		if (pacman.X == pointFruit.X && pacman.Y ==pointFruit){
+			
+		}
+		foodNum = numFood-1;
+	}
 
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
 		
 	}
-    
-    public void keyPressed (KeyEvent e)
-    {
 
-    }
-
-
-    /**
-    * called when a key is released
-    * @param e Thekeyboard event
-    **/
-    public void keyReleased (KeyEvent e)
-    {
-
-    }
-
-
-    /**
-    * called when a character is entered (could be multiple key presses
-    * @param e Thekeyboard event
-    **/public void keyTyped (KeyEvent e)
-    {
-    // this space intentionally left blank
-    }
-     // EventListenerDemo class
 }
