da220de077d7224243503d26da5f5f24f17c43c6
343137220 <343137220@C-3ZNGYR1.tdsb.on.ca>
2017-01-12 15:15:59 -0500

    Starting fruit

--- TheOrganizationName/src/PointFruit.java
+++ TheOrganizationName/src/PointFruit.java
@@ -1,9 +1,25 @@
 
 public class PointFruit {
 	
-	
-	
-	
+	private int counter;
+	private int flashSpeed;
+	public static boolean filledIn;
+	/**
+	 * Calls the superclass constructor, plus sets radius, and flash parameters.
+	 * 
+	 * @param x
+	 *            The x location of fruit.
+	 * @param y
+	 *            The y location of fruit.
+	 * @param left
+	 *            The left edge.
+	 * @param right
+	 *            The right edge.
+	 * @param top
+	 *            The top edge.
+	 * @param bottom
+	 *            The bottom edge.
+	 */
 	
 
 }
