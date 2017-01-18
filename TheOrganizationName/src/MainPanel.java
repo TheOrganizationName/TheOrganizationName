ce0772da92b92460665c5f0f3c9a97b793f989bb
343137220 <343137220@C-3ZNGYR1.tdsb.on.ca>
2017-01-18 14:48:47 -0500

    Point fruit array and display of pictures.

--- TheOrganizationName/src/MainPanel.java
+++ TheOrganizationName/src/MainPanel.java
@@ -8,179 +8,180 @@ import javax.swing.JPanel;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 
-import objects.Student;
 
 public class MainPanel extends JPanel implements Runnable, KeyListener{
 	public int score=0;
 	public int lives=3;
 	int width = 1275;
 	int height = 975;
-	final int numFood = 1000;
-	Pacman pacman=new Pacman( x,  y, left,  right,top, bottom);
+
+
+	final static int numFruits = 1000;
+	public static ArrayList <PointFruit> PointFruit1= new ArrayList<PointFruit>(numFruits);
+
+
+	Pacman pacman=new Pacman( x,  y, left, right, top, bottom);
 	{
-		
+
 	}
-	public static ArrayList <PointFruit> listOfFruit= new ArrayList<PointFruit>(numFood);
+	public static ArrayList <PointFruit> listOfFruit= new ArrayList<PointFruit>(numFruits);
 	PointFruit bananas = new PointFruit();
 	public void pacmanPosition() {
 		// TODO Auto-generated method stub
-		
-	}
-	
-	
 
-	 
+	}
+
+
+
+
 	public void pacmanStatus() {
 		// TODO Auto-generated method stub
 
-}
-	
+	}
+
 
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
-		
+
 	}
 	public void paintComponent(Graphics g) {
 		Font currentFont = g.getFont();
 		Font newFont = currentFont.deriveFont(currentFont.getSize() * 1.4F);
 		g.setFont(newFont);
-			g.drawString("counter: "+score,45,  45);
-			g.drawString("Lives",60,60);
-			
-			if(lives==2)
-			{
+		g.drawString("counter: "+score,45,  45);
+		g.drawString("Lives",60,60);
 
-			}
-			if(lives==1)
-			{
-				
-			}
-			if(lives==0)
-			{
-				
-			}
-		
+		if(lives==2)
+		{
+
 		}
-    
-    public void keyPressed (KeyEvent e)
-    {
-    	int z;
-    	int k;
-    	int r;
-    	int differenceY;
-    	int differenceX;
-    	int hypoteneuse;
-    	int q;
-    	int p;
-    	
+		if(lives==1)
+		{
+
+		}
+		if(lives==0)
+		{
+
+		}
+
+	}
+
+	public void keyPressed (KeyEvent e)
+	{
+		int z;
+		int k;
+		int r;
+		int differenceY;
+		int differenceX;
+		int hypoteneuse;
+		int q;
+		int p;
+
 		int x = (int)pacman.getX();
 		int y = (int)pacman.getY ();
-		
+
 		for( int i=0; i<ghosts.length;i++)
 		{
 			z=(int)ghost[i].getX();
 			k=(int)ghost[i].getY();
 			r=(int)ghost[i].getRadius();
-			
+
 			differenceY= x-z;
 			differenceX=y-k;
-			
+
 			if(differenceY<0|| differenceX<0)
 			{
 				differenceY=differenceY*-1;
 				differenceX= differenceX*-1;
-				
+
 			}
 			p= differenceY;
 			q= differenceX;
 			hypoteneuse=(int) Math.pow((p*p+q*q), 0.5);
 			if(hypoteneuse<(r+1))
 			{
-				
+
 				lives=lives-1;
-				
-				
+
+
 			}
-			
+
 		}
-		for( int i=0; i<fruits.length;i++)
+		for( int i=0; i<PointFruit.size();i++)
 		{
-			z=(int)fruit[i].getX();
-			k=(int)fruit[i].getY();
-			r=(int)fruit[i].getRadius();
-			
+			int z=PointFruit1.getX(i);
+			int k=PointFruit1.getY(i);
+			int r=PointFruit1.getRadius(i);
+
 			differenceY= x-z;
 			differenceX=y-k;
-			
+
 			if(differenceY<0|| differenceX<0)
 			{
 				differenceY=differenceY*-1;
 				differenceX= differenceX*-1;
-				
+
 			}
 			p= differenceY;
 			q= differenceX;
 			hypoteneuse=(int) Math.pow((p*p+q*q), 0.5);
 			if(hypoteneuse<(r+1))
 			{
-				
+
 				score= score+5;
 				fruit[i]=listOfFruit.remove(i);
 			}
-			
+
 		}
-		
+
 		for( int i=0; i<coins.length;i++)
 		{
 			z=(int)coin[i].getX();
 			k=(int)coin[i].getY();
 			r=(int)coin[i].getRadius();
-			
+
 			differenceY= x-z;
 			differenceX=y-k;
-			
+
 			if(differenceY<0|| differenceX<0)
 			{
 				differenceY=differenceY*-1;
 				differenceX= differenceX*-1;
-				
+
 			}
 			p= differenceY;
 			q= differenceX;
 			hypoteneuse=(int) Math.pow((p*p+q*q), 0.5);
 			if(hypoteneuse<(r+1))
 			{
-				
+
 				score= score+5;
 				coin[i]=listOfCoins.remove(i);
 			}
-			
-		}
 
 		}
 
-		
-	
-  
-    
+	}
 
-    public void keyReleased (KeyEvent e)
-     {
- 
-    }
- 
- 
-     /**
-    * called when a character is entered (could be multiple key presses
-     * @param e Thekeyboard event
-    **/public void keyTyped (KeyEvent e)
-    {
-    // this space intentionally left blank
-    }
-     // EventListenerDemo class
-  }
 
-	
 
+
+
+
+	public void keyReleased (KeyEvent e)
+	{
+
+	}
+
+
+	/**
+	 * called when a character is entered (could be multiple key presses
+	 * @param e Thekeyboard event
+	 **/public void keyTyped (KeyEvent e)
+	 {
+		 // this space intentionally left blank
+	 }
+	 // EventListenerDemo class
+}
 
